17e820e88a8cb95eaf9c4339cc555efdb32d734c
YoadRX <yoadrimok@hilma.tech>
2023-10-16 17:49:59 +0300

    Validation of the email and password of the Sign in

--- SignIn/js/script.js
+++ SignIn/js/script.js
@@ -1,24 +1,27 @@
 const email = document.querySelectorAll(".email");
 let submit = document.querySelector(".submit2");
 let checkBox = document.querySelector(".chk-box");
-        // if(localStorage.getItem()
+const users = localStorage.getItem("usersArray")
+let usersArray = JSON.parse(users);
+const currentUser = localStorage.getItem("currentUser")
+let currentUserArray = JSON.parse(currentUser);
+
+console.log(usersArray);
 function verifyPassword() {
     var pw = document.getElementById("pswd").value;
-    //check empty password field
-    if(pw == "") {
-        alert("is nothing there")
-       return false;
+   //minimum password length validation and password
+    if(currentUserArray.email == email.item(0).value && currentUserArray.password == pw && !(pw.length < 8 && pw == "")){
+      return true;
     }
-   
-   //minimum password length validation
-    if(pw.length < 8) {
-        // 
-        alert("your password is too short")
-       return false;
+    else{
+      alert("your password is empty or too short")
+      return false;
     }
   }
-submit.addEventListener("click", function(){    
-    if(checkBox.checked == true){
-        // console.log("hey there");
-      }
-});
+  submit.addEventListener("click", function(){    
+    console.log();
+  
+      if(checkBox.checked == true){
+          // console.log("hey there");
+        } 
+  });
