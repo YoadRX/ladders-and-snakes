b641614dc9acc4214b6506a9ff4caa4469c6b24b
shanimashmush <shanimashmush@hilma.tech>
2023-10-16 19:09:50 +0300

    start of localstorage

--- SignIn/js/script.js
+++ SignIn/js/script.js
@@ -1,24 +1,34 @@
 const email = document.querySelectorAll(".email");
 let submit = document.querySelector(".submit2");
 let checkBox = document.querySelector(".chk-box");
-        // if(localStorage.getItem()
+// if(localStorage.getItem()
+
+const usersArray = JSON.parse(localStorage.getItem('usersArray'));
+
+
+
 function verifyPassword() {
-    var pw = document.getElementById("pswd").value;
-    //check empty password field
-    if(pw == "") {
-        alert("is nothing there")
-       return false;
-    }
-   
-   //minimum password length validation
-    if(pw.length < 8) {
-        // 
-        alert("your password is too short")
-       return false;
-    }
+  const pw = document.getElementById("pswd").value;
+  //check empty password field
+  if (pw == "") {
+    alert("is nothing there")
+    return false;
   }
-submit.addEventListener("click", function(){    
-    if(checkBox.checked == true){
-        // console.log("hey there");
-      }
+
+  //minimum password length validation
+  if (pw.length < 6) {
+    // 
+    alert("your password is too short")
+    return false;
+  }
+  for (i = 0; i < usersArray; i++) {
+    // for (i) {
+    //   console.log("hi");
+    // }
+  }
+}
+submit.addEventListener("click", function () {
+  if (checkBox.checked == true) {
+    // console.log("hey there");
+  }
 });
