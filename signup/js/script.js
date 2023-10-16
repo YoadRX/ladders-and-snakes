fa73a4c1901c2f9c85e6a6adaf5e609d4d420fe2
shanimashmush <shanimashmush@hilma.tech>
2023-10-16 13:05:11 +0300

    added more validation

--- signup/js/script.js
+++ signup/js/script.js
@@ -1,26 +1,44 @@
 const repeatPassword = document.getElementById('repeatPassword');
+const password = document.getElementById('password');
 const errorMessage = document.getElementsByClassName('errorMessageFalse');
 const emailInput = document.getElementById('email');
+const username = document.getElementById('username');
+//all the elements from html
+
 
 const createUser = (e) => {
     e.preventDefault()
     repeatPassword.classList.remove('error');
-    console.log(errorMessage[0].classList);
-    errorMessage[0].classList.remove('errorMessageTrue');
+    password.classList.remove('error');
+    username.classList.remove('error');
     emailInput.classList.remove('error');
-    errorMessage[1].classList.remove('errorMessageTrue');
+    for (i = 0; i < errorMessage.length; i++){
+        errorMessage[i].classList.remove('errorMessageTrue');
+    }
+    //remove all error class from all elements
+    
 
-    if (e.target[2].value !== e.target[3].value || emailInput.value === '') {
+    if (e.target[2].value !== e.target[3].value || emailInput.value === '' || username.value.length < 6 || password.value < 6) {
         if (e.target[2].value !== e.target[3].value) {
             repeatPassword.classList.add("error");
-            errorMessage[1].classList.add("errorMessageTrue");
+            errorMessage[3].classList.add("errorMessageTrue");
         }
         if (emailInput.value === '') {
             emailInput.classList.add("error");
+            errorMessage[1].classList.add("errorMessageTrue");
+        }
+        if (username.value.length < 5) {
+            username.classList.add("error");
             errorMessage[0].classList.add("errorMessageTrue");
+        }
+        if (password.value.length < 5){
+            password.classList.add("error");
+            errorMessage[2].classList.add("errorMessageTrue");
         }
         return "error";
     }
+    //checks if the user inputs are valid, if not it adds error classes to the right elements
+
     let usersArray = JSON.parse(localStorage.getItem("usersArray"));
     usersArray.push({
         userName: e.target[0].value,
@@ -29,9 +47,11 @@ const createUser = (e) => {
     })
     localStorage.setItem('usersArray', JSON.stringify(usersArray));
     console.log(errorMessage[0].classList);
+    //assd the user inputs as an object in the arry in the local storage
 }
 
 const signUpForm = document.getElementById("signUp");
 signUpForm.onsubmit = createUser;
+//activate the function once it is submitted
 
 
