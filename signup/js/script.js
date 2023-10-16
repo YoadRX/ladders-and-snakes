7f232f26679fb0c3853cb7db899f9b571171bf2b
shanimashmush <shanimashmush@hilma.tech>
2023-10-16 12:22:27 +0300

    added error detctor

--- signup/js/script.js
+++ signup/js/script.js
@@ -1,9 +1,26 @@
-// const submitSignUp = document.getElementById("submitSignUp");
+const repeatPassword = document.getElementById('repeatPassword');
+const errorMessage = document.getElementsByClassName('errorMessageFalse');
+const emailInput = document.getElementById('email');
 
 const createUser = (e) => {
     e.preventDefault()
-    // console.log('✌️e --->', e.target[0].value);
+    repeatPassword.classList.remove('error');
+    console.log(errorMessage[0].classList);
+    errorMessage[0].classList.remove('errorMessageTrue');
+    emailInput.classList.remove('error');
+    errorMessage[1].classList.remove('errorMessageTrue');
 
+    if (e.target[2].value !== e.target[3].value || emailInput.value === '') {
+        if (e.target[2].value !== e.target[3].value) {
+            repeatPassword.classList.add("error");
+            errorMessage[1].classList.add("errorMessageTrue");
+        }
+        if (emailInput.value === '') {
+            emailInput.classList.add("error");
+            errorMessage[0].classList.add("errorMessageTrue");
+        }
+        return "error";
+    }
     let usersArray = JSON.parse(localStorage.getItem("usersArray"));
     usersArray.push({
         userName: e.target[0].value,
@@ -11,12 +28,10 @@ const createUser = (e) => {
         password: e.target[2].value
     })
     localStorage.setItem('usersArray', JSON.stringify(usersArray));
-
+    console.log(errorMessage[0].classList);
 }
+
 const signUpForm = document.getElementById("signUp");
 signUpForm.onsubmit = createUser;
-localStorage.setItem('usersArray', '[]');
-function getUserDataToLocalStorage() {
-    // console.log('✌️usersArray --->', usersArray);
 
-}
+
